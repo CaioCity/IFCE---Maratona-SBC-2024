a35b567bfaad3df1eead7bb0277b583ec7b9ac5b
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-04-24 22:07:03 -0300

    Add files via upload

---
+++ Semana_1/leet_Pascal's_Triangle_E.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/pascals-triangle/description/
+// Problema: Pascal's Triangle
+// Inserir code diretamente no leet
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector <vector<int> > triangle;
+        vector <int> linha1;
+        linha1.push_back(1);
+        triangle.push_back(linha1);
+       
+        for(int i=1; i < numRows; i++){
+            vector <int> linha;
+            linha.push_back(1);
+            for(int j=1; j<i; j++){
+                linha.push_back(triangle[i-1][j-1]+triangle[i-1][j]);
+            }
+            linha.push_back(1);
+            triangle.push_back(linha);
+        }
+        return triangle;
+    }
+    
+};

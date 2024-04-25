3b5a3ca79b0564ee3b541b5d26b8fa548b4aa39c
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-04-24 21:51:39 -0300

    Add files via upload

---
+++ Semana_1/bcwd2413_A.cpp
@@ -0,0 +1,13 @@
+// https://judge.beecrowd.com/en/problems/view/2413
+// Problema 2413: Busca na Internet
+
+#include <iostream>
+ 
+int main() {
+ 
+    unsigned short int t;
+    std::cin>>t;
+    std::cout<<t*4<<std::endl;
+ 
+    return 0;
+}

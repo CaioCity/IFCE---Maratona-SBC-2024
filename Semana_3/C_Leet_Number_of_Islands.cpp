a1714ae3d221b98aa5fe17bf4a9db9a496ac0b1b
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-05-08 09:40:59 -0300

    Update C_Leet_Number_of_Islands.cpp

--- Semana_3/C_Leet_Number_of_Islands.cpp
+++ Semana_3/C_Leet_Number_of_Islands.cpp
@@ -3,26 +3,28 @@
 
 class Solution {
 public:
-    void define_ilha(short int i, short int j, vector<vector<char>>& grid,
-                     bool visited[][310]) {
+    void define_ilha(short int i, short int j, vector<vector<char>>& grid) {
         queue<pair<short int, short int>> fila;
         fila.push(make_pair(i, j));
 
         while (!(fila.empty())) {
             short int a = fila.front().first, b = fila.front().second;
             fila.pop();
-            if (visited[a][b])
-                continue;
-            visited[a][b] = 1;
-
-            if (grid[a][b + 1] && !(visited[a][b + 1]))
-                fila.push(make_pair(a, (b + 1)));
-            if (grid[a][b - 1] && !(visited[a][b - 1]))
-                fila.push(make_pair(a, (b - 1)));
-            if (grid[a + 1][b] && !(visited[a + 1][b]))
-                fila.push(make_pair((a + 1), b));
-            if (grid[a - 1][b] && !(visited[a - 1][b]))
-                fila.push(make_pair((a - 1), b));
+            
+            grid[a][b] = 0;
+            short int y = grid.size()-1, x = grid[0].size()-1;
+            if(b<x)
+                if (grid[a][b + 1])
+                    fila.push(make_pair(a, (b + 1)));
+            if(b>0)
+                if (grid[a][b - 1])
+                    fila.push(make_pair(a, (b - 1)));
+            if(a<y)
+                if (grid[a + 1][b])
+                    fila.push(make_pair((a + 1), b));
+            if(a>0)
+                if (grid[a - 1][b])
+                    fila.push(make_pair((a - 1), b));
         }
     }
 
@@ -30,17 +32,12 @@ public:
 
         int ilhas = 0;
         short int y = grid.size(), x = grid[0].size();
-        bool visited[310][310];
-
-        for (short int i = 0; i < y; i++)
-            for (short int j = 0; j < y; j++)
-                visited[i][j] = 0;
 
         for (short int i = 0; i < y; i++)
             for (short int j = 0; j < x; j++)
-                if (grid[i][j] && !visited[i][j]) {
+                if (grid[i][j]) {
                     ilhas++;
-                    define_ilha(i, j, grid, visited);
+                    define_ilha(i, j, grid);
                 }
 
         return ilhas;

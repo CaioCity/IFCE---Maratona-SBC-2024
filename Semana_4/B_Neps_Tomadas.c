02659190e59e0e746c3460d9dcc001be7ba3a6c6
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-05-15 09:43:12 -0300

    Create B_Neps_Tomadas.c

---
+++ Semana_4/B_Neps_Tomadas.c
@@ -0,0 +1,13 @@
+// https://neps.academy/br/exercise/219
+// Tomadas
+
+#include <stdio.h>
+
+int main(){
+    
+    short int a,b,c,d;
+    scanf("%hd%hd%hd%hd",&a,&b,&c,&d);
+    printf("%hd\n",a+b+c+d-3);
+  
+    return 0;
+}

f036e4d62569b6f24c69045140064d6bf0096d78
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-05-19 20:17:24 -0300

    Update A_Neps_Jogo_de_Par_ou_Impar.cpp

--- Semana_5/A_Neps_Jogo_de_Par_ou_Impar.cpp
+++ Semana_5/A_Neps_Jogo_de_Par_ou_Impar.cpp
@@ -1,1 +1,28 @@
+// https://neps.academy/br/exercise/51
+// Jogo de Par ou Ímpar
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int p,d1,d2;
+
+int main() {
+
+  cin>>p>>d1>>d2;
+
+  d1+=d2;
+  if(p==0){
+    if(d1%2==0){
+      cout<<"0"<<endl;
+    }else{
+      cout<<"1"<<endl;
+    }
+  }else{
+    if(d1%2==0){
+      cout<<"1"<<endl;
+    } else{
+      cout<<"0"<<endl;
+    }
+  }
+    return 0;
+}

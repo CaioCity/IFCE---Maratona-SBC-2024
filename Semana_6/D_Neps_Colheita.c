d87db53fe0347616e5386b575572f862ad5503d4
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-05-28 11:51:40 -0300

    Update D_Neps_Colheita.c

--- Semana_6/D_Neps_Colheita.c
+++ Semana_6/D_Neps_Colheita.c
@@ -1,50 +1,9 @@
 // https://neps.academy/br/exercise/2698
 // Colheita
 
-// Duas soluções (a ideia é a mesma, em teoria a segunda está mais otimizada)
+// Duas soluções (a ideia é a mesma, primeira apenas está mais otimizada)
 
 // Primeira:
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-
-    short int N,M,maximo=0,aux;
-    scanf("%hd%hd",&N,&M);
-
-    short int terra[N][M], saldo_ao_sair[N][M];
-    
-    for(short int i=0; i<N; i++)
-        for(short int j=0; j<M; j++){
-            scanf("%hd",&terra[i][j]);
-            saldo_ao_sair[i][j]=(-1)*terra[i][j];
-        }
-        
-    for(short int i=0; i<N; i++)
-        for(short int j=0; j<M; j++){
-            aux=0;
-            for(short a=i-1; a>=0; a--)
-                if(terra[i][j]>saldo_ao_sair[a][j])
-                    aux= aux>(saldo_ao_sair[a][j]+terra[i][j])? aux : (saldo_ao_sair[a][j]+terra[i][j]);
-                saldo_ao_sair[i][j]=aux-terra[i][j];
-            aux=0;
-            for(short b=j-1; b>=0; b--)
-                if(terra[i][j]>saldo_ao_sair[i][b])
-                    aux= aux>(saldo_ao_sair[i][b]+terra[i][j])? aux : (saldo_ao_sair[i][b]+terra[i][j]);
-                saldo_ao_sair[i][j]= saldo_ao_sair[i][j]>(aux-terra[i][j])? saldo_ao_sair[i][j] : (aux-terra[i][j]);
-        }
-
-    for(short int i=0; i<N; i++)
-        for(short int j=0; j<M; j++)    
-            if( (saldo_ao_sair[i][j] + terra[i][j]) > maximo)
-                maximo= saldo_ao_sair[i][j]+terra[i][j];
-            
-    printf("%hd\n",maximo);
-    
-    return 0;
-}
-
-// Segunda:
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -84,9 +43,50 @@ int main(){
         
     for(short int i=0; i<N; i++)
         for(short int j=0; j<M; j++)    
-            if( (saldo_ao_sair[i][j] + terra[i][j]) > maximo && saldo_ao_sair[i][j]!=((-1)*terra[i][j]) ){
+            if( (saldo_ao_sair[i][j] + terra[i][j]) > maximo && saldo_ao_sair[i][j]!=((-1)*terra[i][j]) )
                 maximo= saldo_ao_sair[i][j]+terra[i][j];
                      
+    printf("%hd\n",maximo);
+    
+    return 0;
+}
+
+// Segunda:
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    short int N,M,maximo=0,aux;
+    scanf("%hd%hd",&N,&M);
+
+    short int terra[N][M], saldo_ao_sair[N][M];
+    
+    for(short int i=0; i<N; i++)
+        for(short int j=0; j<M; j++){
+            scanf("%hd",&terra[i][j]);
+            saldo_ao_sair[i][j]=(-1)*terra[i][j];
+        }
+        
+    for(short int i=0; i<N; i++)
+        for(short int j=0; j<M; j++){
+            aux=0;
+            for(short a=i-1; a>=0; a--)
+                if(terra[i][j]>saldo_ao_sair[a][j])
+                    aux= aux>(saldo_ao_sair[a][j]+terra[i][j])? aux : (saldo_ao_sair[a][j]+terra[i][j]);
+                saldo_ao_sair[i][j]=aux-terra[i][j];
+            aux=0;
+            for(short b=j-1; b>=0; b--)
+                if(terra[i][j]>saldo_ao_sair[i][b])
+                    aux= aux>(saldo_ao_sair[i][b]+terra[i][j])? aux : (saldo_ao_sair[i][b]+terra[i][j]);
+                saldo_ao_sair[i][j]= saldo_ao_sair[i][j]>(aux-terra[i][j])? saldo_ao_sair[i][j] : (aux-terra[i][j]);
+        }
+
+    for(short int i=0; i<N; i++)
+        for(short int j=0; j<M; j++)    
+            if( (saldo_ao_sair[i][j] + terra[i][j]) > maximo)
+                maximo= saldo_ao_sair[i][j]+terra[i][j];
+            
     printf("%hd\n",maximo);
     
     return 0;

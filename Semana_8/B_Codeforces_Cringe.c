0c5b4d0b75bef58690b20699863cbe25dbee8646
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-17 09:04:19 -0300

    Create B_Codeforces_Cringe.c

---
+++ Semana_8/B_Codeforces_Cringe.c
@@ -0,0 +1,11 @@
+// https://codeforces.com/group/YgJmumGtHD/contest/528947/problem/A
+// Cringe
+
+#include <stdio.h> 
+
+int main(){
+    short int Y;
+    scanf("%hd",&Y);
+    printf("%hd\n", 2024-(2024-Y)*2);
+    return 0;
+}
